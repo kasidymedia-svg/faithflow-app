d9c15e050a0fd7b7662e84b3eabfd66493aea161
kasidymedia-svg <kasidymedia@gmail.com>
2025-10-14 19:24:49 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='faithflow-v1';
-const URLs=['/','/index.html','/style.css','/script.js','/manifest.json','/icons/icon-192.png','/icons/icon-512.png','/splash/splash-1080x1920.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLs)));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
